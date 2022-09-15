73f09520373e3932e89a4874ad7ee28b5e850816
Dimitri <dimitri.justeau@gmail.com>
2022-09-15 13:53:37 +0200

    Try to fix win build

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ elif [ "$OS" = "Darwin" ]; then
     native-image --shared -H:Name=libchoco_capi -cp choco-solver-capi-1.0-SNAPSHOT.jar --no-fallback --link-at-build-time
 else
     native-image.cmd --shared -H:Name=libchoco_capi -cp choco-solver-capi-1.0-SNAPSHOT.jar --no-fallback --link-at-build-time
+    mv libchoco_capi.dll choco_capi.dll
     mv libchoco_capi.lib choco_capi.lib
 fi
 
